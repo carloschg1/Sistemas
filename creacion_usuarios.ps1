75e551451342bb51cfd62e29df9f1ae170f119b0
carloschg1 <115078611+carloschg1@users.noreply.github.com>
2022-12-28 14:24:32 +0100

    Delete creacion_usuarios.ps1

--- creacion_usuarios.ps1
+++
@@ -1,9 +0,0 @@
-$file_users=Import-Csv -Path empleados.csv 
-foreach ($user in $file_users) { 
-  $clave=ConvertTo-SecureString $user.password -AsPlainText -Force
-  New-LocalUser $user.cuenta -Password $clave -FullName $user.nombre_apellido -Description $user.descripcion -AccountNeverExpires  
-  net user $user.cuenta /logonpasswordchg:yes
-  #AÃ±adimos la cuenta de usuario en el grupo de Usuarios del sistema
-  Add-LocalGroupMember -Group $user.departamentos -Member $user.cuenta 
-  Add-LocalGroupMember -Group Usuarios -Member $user.cuenta 
-}
