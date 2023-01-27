164166ba95305ca34b6cf65f47f5ff6ab8750fec
carloschg1 <115078611+carloschg1@users.noreply.github.com>
2023-01-27 12:15:41 +0100

    Create directorios.sh

---
+++ directorios.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mkdir -p proyecto/{pruebas,src} && touch proyecto/{Proyecto1.pdf,Proyecto2.pdf} && touch proyecto/pruebas/{prueba1.pdf,prueba2.pdf} && touch proyecto/src/{app1.sh,app2.sh}
