9957fda0344b95886c98c9e2a22ce5c898eb2d3d
carloschg1 <115078611+carloschg1@users.noreply.github.com>
2023-01-28 16:20:30 +0100

    Update ampliacion.sh

--- ampliacion.sh
+++ ampliacion.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #Crear el grupo de diseño de interfaces
 
-groupadd diseinterf
+groupadd diseinter
 
 #Añadir a algún usuario a este grupo
 
-adduser usu_diseinterf
-usermod -a -G diseinterf usu_diseinterf
+adduser usu_diseinter
+usermod -a -G diseinter usu_diseinter
 
 #Crear las acls pertinentes
-setfacl -R -m g:diseinterf:r,d:g:diseinterf:r /mnt/publico/proyecto/pruebas &&
-setfacl -R -m g:diseinterf:r,d:g:diseinterf:r /mnt/publico/proyecto/src &&
-setfacl -m g:diseinterf:rx /mnt/publico/proyecto/ &&
-setfacl -m g:diseinterf:rx /mnt/publico/proyecto/src &&
-setfacl -m g:diseinterf:rx /mnt/publico/proyecto/pruebas
+setfacl -R -m g:diseinter:r,d:g:diseinter:r /mnt/publico/proyecto/pruebas &&
+setfacl -R -m g:diseinter:r,d:g:diseinter:r /mnt/publico/proyecto/src &&
+setfacl -m g:diseinter:rx /mnt/publico/proyecto/ &&
+setfacl -m g:diseinter:rx /mnt/publico/proyecto/src &&
+setfacl -m g:diseinter:rx /mnt/publico/proyecto/pruebas
 
 
 
